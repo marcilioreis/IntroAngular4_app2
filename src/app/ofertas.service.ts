a95f22acddda85e2f49514c6ddaf8a3ebda77e9a
MARCILIOREIS\email <emaildomarcilio@gmail.com>
2018-06-19 22:56:30 -0300

    Rotas e Navegação: Como usar, Onde fica

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -15,23 +15,39 @@ export class OfertasService {
 
     public getOfertas(): Promise<Oferta[]> {
         // efetuar uma requisição http
-        return this.http.get(`${URL_API}?destaque=true`)
+        return this.http.get(`${URL_API}/ofertas?destaque=true`)
             .toPromise()
             .then((resposta: any) => resposta.json());
         // retornar uma promisse OFERTA{}
     }
 
     public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
-        return this.http.get(`${URL_API}?categoria=${categoria}`)
+        return this.http.get(`${URL_API}/ofertas?categoria=${categoria}`)
             .toPromise()
             .then((resposta: any) => resposta.json());
     }
 
     public getOfertaPorId(id: number): Promise<Oferta> {
-        return this.http.get(`${URL_API}?id=${id}`)
+        return this.http.get(`${URL_API}/ofertas?id=${id}`)
             .toPromise()
             .then((resposta: any) =>  {
                 return resposta.json()[0];
+            });
+    }
+
+    public getComoUsarOfertaPorId(id: number): Promise<string> {
+        return this.http.get(`${URL_API}/como-usar?id=${id}`)
+            .toPromise()
+            .then((resposta: any) =>  {
+                return resposta.json()[0].descricao;
+            });
+    }
+
+    public getOndeFicaOfertaPorId(id: number): Promise<string> {
+        return this.http.get(`${URL_API}/onde-fica?id=${id}`)
+            .toPromise()
+            .then((resposta: any) =>  {
+                return resposta.json()[0].descricao;
             });
     }
 }
