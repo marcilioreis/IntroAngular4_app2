f6d1e369f1a2f20afac0d3b14a2daf235c4dfc17
MARCILIOREIS\email <emaildomarcilio@gmail.com>
2018-06-19 21:41:42 -0300

    Rotas e Navegação: Passando parâmetros

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Oferta } from '../shared/oferta.model';
+import { OfertasService } from '../ofertas.service';
 
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
-  styleUrls: ['./oferta.component.css']
+  styleUrls: ['./oferta.component.css'],
+  providers: [ OfertasService ]
 })
 export class OfertaComponent implements OnInit {
 
-  constructor() { }
+  public oferta: Oferta;
+
+ constructor(
+   private route: ActivatedRoute,
+   private ofertasService: OfertasService
+  ) {  }
 
   ngOnInit() {
+    // smapshot
+    // console.log('ID recuperado da rota: ',  this.route.snapshot.params['id']);
+
+    // subscribe
+      /* this.route.params.subscribe((parametro: any) => {
+        console.log(parametro);
+      }); */
+
+      this.ofertasService.getOfertaPorId(this.route.snapshot.params['id'])
+        .then(( oferta: Oferta ) => {
+          this.oferta = oferta;
+        });
   }
 
 }
